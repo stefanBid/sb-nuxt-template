28ef18380ee655ffeb96cb9d31a991966d13be12
Stefano Biddau <44703164+stefanBid@users.noreply.github.com>
2025-11-20 00:58:25 +0100

    feat: add i18n settings (#8)
    * fix: reorder dependencies in package.json for better organization
    
    * feat(i18n): add English and Italian localization files
    
    * feat(i18n): integrate i18n for header and layout components
    
    * feat(i18n): configure i18n settings with English and Italian locales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@vueuse/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    '@nuxtjs/i18n',
+  ],
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
@@ -14,6 +20,22 @@ export default defineNuxtConfig({
   eslint: {
     config: {
       stylistic: true,
+    },
+  },
+  i18n: {
+    baseUrl: 'http://localhost:3000/',
+    strategy: 'prefix_except_default',
+    defaultLocale: 'en',
+
+    locales: [
+      { code: 'en', iso: 'en-US', language: 'en-US', name: 'English', file: 'en.json' },
+      { code: 'it', iso: 'it-IT', language: 'it-IT', name: 'Italiano', file: 'it.json' },
+    ],
+    langDir: 'locales/',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
     },
   },
   icon: {
