45ce91279c98466cb4bd469797948b5c28140bd1
Stefano Biddau <44703164+stefanBid@users.noreply.github.com>
2025-11-19 23:29:17 +0100

    Feat/add style configuration (#4)
    * feat: aggiungere Tailwind CSS e configurazione Vite nel progetto
    
    * feat: aggiungere configurazione di Tailwind CSS in nuxt.config.ts
    
    * feat: aggiungere configurazione di stili e transizioni con Tailwind CSS
    
    * feat: aggiornare configurazione di stili e transizioni, rinominare variabili e modificare il file package-lock.json
    
    * feat: update selection style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       stylistic: true,
