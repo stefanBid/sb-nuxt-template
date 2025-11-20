3b220e56257bdb97e98bb11b994e618569bf98c2
Stefano Biddau <44703164+stefanBid@users.noreply.github.com>
2025-11-20 10:07:24 +0100

    feat: add notifications (#14)
    * feat: implement notification system with add, remove, and clear functionalities
    
    * feat: add notification banner and box components
    
    * feat: add notification handling and close button component
    
    * feat: remove focus ring styles from close button component

---
+++ app/components/base/close-button/BaseCloseButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// Input / Output
+const emit = defineEmits <{
+  // eslint-disable-next-line no-unused-vars
+  (e: 'close', falsyValue: false): void
+}>()
+
+// Events
+const onClose = () => {
+  emit('close', false)
+}
+</script>
+
+<template>
+  <button
+    class="inline-flex group items-center rounded-xl p-1 ml-4 cursor-pointer text-app-muted hover:text-app-contrast u-app-soft-transition"
+    @click.stop="onClose"
+  >
+    <Icon
+      class="size-6 md:size-7 u-app-soft-transition"
+      name="solar:close-circle-bold-duotone"
+    />
+  </button>
+</template>
