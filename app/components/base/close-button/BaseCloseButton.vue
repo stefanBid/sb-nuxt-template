7f5e096a95819f4f9b3d6076bb7c7f389a3848e6
stefanBid <biddau.stefano99@gmail.com>
2025-11-20 10:05:56 +0100

    feat: remove focus ring styles from close button component

--- app/components/base/close-button/BaseCloseButton.vue
+++ app/components/base/close-button/BaseCloseButton.vue
@@ -13,7 +13,7 @@ const onClose = () => {
 
 <template>
   <button
-    class="inline-flex group items-center rounded-xl p-1 focus:outline-none focus:ring-2 focus:ring-app-accent ml-4 cursor-pointer text-app-muted hover:text-app-contrast u-app-soft-transition"
+    class="inline-flex group items-center rounded-xl p-1 ml-4 cursor-pointer text-app-muted hover:text-app-contrast u-app-soft-transition"
     @click.stop="onClose"
   >
     <Icon
